5ab851f503769029852d0292e291079f5779f0cd
annapetrosyaan <89910608+annapetrosyaan@users.noreply.github.com>
2025-12-24 18:30:45 +0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,9 +2,5 @@ const menuIcon = document.getElementById('mobile-menu-icon');
 const mobileNav = document.getElementById('mobile-nav');
 
 menuIcon.addEventListener('click', () => {
-  if (mobileNav.style.display === 'flex') {
-    mobileNav.style.display = 'none';
-  } else {
-    mobileNav.style.display = 'flex';
-  }
+  mobileNav.style.display = mobileNav.style.display === 'flex' ? 'none' : 'flex';
 });
